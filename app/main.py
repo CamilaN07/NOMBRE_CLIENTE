e28af68bc6cf07ce1da46a0377f9de95ab1d4ce6
CamilaN07 <mariacamilaninosalamanca@gmail.com>
2026-07-03 02:12:41 -0500

    listas clientes, transacciones, facturas

--- app/main.py
+++ app/main.py
@@ -19,3 +19,21 @@ Factura.model_rebuild()
 transaccion.model_rebuild()
 facturaleer.model_rebuild()
 facturaleercompuesta.model_rebuild()
+
+
+app = FastAPI(lifespan=crear_tablas)
+
+
+
+lista_clientes: list[Cliente] = []
+lista_transacciones: list[TransaccionBase] = []
+lista_facturas: list[Factura] = []
+ 
+#incluir el enrutador de clientes
+app.include_router(clientes.rutas_clientes, tags=["Clientes"])
+
+#incluir el enrutador de facturas
+app.include_router(facturas.rutas_facturas, tags=["Facturas"])
+
+#incluir el enrutador de transacciones
+app.include_router(transacciones.rutas_transacciones, tags=["Transacciones"])
